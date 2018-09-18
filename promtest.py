53f420e246963f9a1a9ec3ce345cb00d4532bc6f
Andrew Nelson <skamphax0r@localhost.localdomain>
2018-09-17 20:11:25 -0500

    fixes

--- promtest.py
+++ promtest.py
@@ -1,16 +1,15 @@
+"""testing prometheus stuff"""
+import time
+import datetime
+import urllib
 from timeit import default_timer as timer
 from prometheus_client import start_http_server, Summary, Gauge
-import time
-import datetime 
-import random
-import urllib
 
-# Create a metric to track time spent and requests made.
 LATENCY_TIME = Gauge('latency_getting_google', 'latency to google')
 LATENCY_LOCAL = Gauge('local_latency', 'hitting localhost')
 def get_latency():
     """a function to grab latency to google.com"""
-    url='http://google.com'
+    url = 'http://google.com'
     nf = urllib.request.urlopen(url)
     start = datetime.datetime.now()
     page = nf.read()
@@ -19,12 +18,11 @@ def get_latency():
     delta = end - start
     print(int(delta.total_seconds() * 1000))
     LATENCY_TIME.set(float(delta.total_seconds() * 1000))
-    # end - start gives you the page load time
 
 
 def get_latency_local():
     """a function to grab latency to localhost"""
-    url='http://localhost:8090'
+    url = 'http://localhost:8090'
     nf = urllib.request.urlopen(url)
     start = datetime.datetime.now()
     page = nf.read()
@@ -33,12 +31,12 @@ def get_latency_local():
     delta = end - start
     print(int(delta.total_seconds() * 1000))
     LATENCY_LOCAL.set(float(delta.total_seconds() * 1000))
-    # end - start gives you the page load time
+
 
 if __name__ == '__main__':
     # Start up the server to expose the metrics.
     start_http_server(8090)
-    # Generate some requests.
+    # populate some metrics
     while True:
         get_latency()
         get_latency_local()
